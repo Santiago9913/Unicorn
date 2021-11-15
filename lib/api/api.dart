f849753956c11dcd6281d9e1911786482763d6eb
Santiago <s.fajardot@uniandes.edu.co>
2021-11-14 23:48:15 -0500

    Finished

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:unicorn/controllers/api_controller.dart';
 import 'package:unicorn/models/country.dart';
 import 'package:unicorn/models/trend.dart';
@@ -7,7 +8,6 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Api {
-
   ApiController mApi = ApiController();
 
   Future<List<Country>> getCountries() async {
@@ -18,17 +18,19 @@ class Api {
       await prefs.setString('countries', json.encode(response));
       List countries = response['data'];
       List<Country> mCountries = <Country>[];
-      for (int i = 0; i < countries.length; i++) {
-        mCountries.add(new Country(name: countries[i]));
-      }
+      // for (int i = 0; i < countries.length; i++) {
+      //   mCountries.add(new Country(name: countries[i]));
+      // }
+      mCountries = await compute(getCountriesIsolate, countries);
       return mCountries;
     } else {
       final response = json.decode(mCountriesCache);
       List countries = response['data'];
       List<Country> mCountries = <Country>[];
-      for (int i = 0; i < countries.length; i++) {
-        mCountries.add(new Country(name: countries[i]));
-      }
+      // for (int i = 0; i < countries.length; i++) {
+      //   mCountries.add(new Country(name: countries[i]));
+      // }
+      mCountries = await compute(getCountriesIsolate, countries);
       return mCountries;
     }
   }
@@ -39,25 +41,33 @@ class Api {
     if (mTrendsCache == '') {
       final response = await mApi.fetchTrends(country);
       await prefs.setString('trend' + country, json.encode(response));
-      var tempTrends = response['data'].keys;
-      List trends = [];
-      for (var mTrend in tempTrends) trends.add(mTrend);
+      // var tempTrends = response['data'].keys;
+      // List trends = [];
+      // for (var mTrend in tempTrends) trends.add(mTrend);
       List<Trend> mTrends = <Trend>[];
-      for (int i = 0; i < trends.length; i++) {
-        var mTrend = response['data'][trends[i]];
-        mTrends.add(new Trend(name: trends[i], funding: mTrend['funding'], startups: mTrend['startups']));
-      }
+      // for (int i = 0; i < trends.length; i++) {
+      //   var mTrend = response['data'][trends[i]];
+      //   mTrends.add(new Trend(
+      //       name: trends[i],
+      //       funding: mTrend['funding'],
+      //       startups: mTrend['startups']));
+      // }
+      mTrends = await compute(getTrendsIsolate, response);
       return mTrends;
     } else {
       final response = json.decode(mTrendsCache);
-      var tempTrends = response['data'].keys;
-      List trends = [];
-      for (var mTrend in tempTrends) trends.add(mTrend);
+      // var tempTrends = response['data'].keys;
+      // List trends = [];
+      // for (var mTrend in tempTrends) trends.add(mTrend);
       List<Trend> mTrends = <Trend>[];
-      for (int i = 0; i < trends.length; i++) {
-        var mTrend = response['data'][trends[i]];
-        mTrends.add(new Trend(name: trends[i], funding: mTrend['funding'], startups: mTrend['startups']));
-      }
+      // for (int i = 0; i < trends.length; i++) {
+      //   var mTrend = response['data'][trends[i]];
+      //   mTrends.add(new Trend(
+      //       name: trends[i],
+      //       funding: mTrend['funding'],
+      //       startups: mTrend['startups']));
+      // }
+      mTrends = await compute(getTrendsIsolate, response);
       return mTrends;
     }
   }
@@ -68,26 +78,76 @@ class Api {
     if (mInversionCache == '') {
       final response = await mApi.fetchInversion();
       await prefs.setString('inversion', json.encode(response));
-      var tempIndustries = response['data'].keys;
-      List industries = [];
-      for (var mIndustry in tempIndustries) industries.add(mIndustry);
+      // var tempIndustries = response['data'].keys;
+      // List industries = [];
+      // for (var mIndustry in tempIndustries) industries.add(mIndustry);
       List<Industry> mIndustries = <Industry>[];
-      for (int i = 0; i < industries.length; i++) {
-        var mIndustry = response['data'][industries[i]];
-        mIndustries.add(new Industry(name: industries[i], funding: mIndustry['funding'], startups: mIndustry['startups']));
-      }
+      // for (int i = 0; i < industries.length; i++) {
+      //   var mIndustry = response['data'][industries[i]];
+      //   mIndustries.add(new Industry(
+      //       name: industries[i],
+      //       funding: mIndustry['funding'],
+      //       startups: mIndustry['startups']));
+      // }
+      mIndustries = await compute(getInversionsIsolate, response);
       return mIndustries;
     } else {
       final response = json.decode(mInversionCache);
-      var tempIndustries = response['data'].keys;
-      List industries = [];
-      for (var mIndustry in tempIndustries) industries.add(mIndustry);
+      // var tempIndustries = response['data'].keys;
+      // List industries = [];
+      // for (var mIndustry in tempIndustries) industries.add(mIndustry);
       List<Industry> mIndustries = <Industry>[];
-      for (int i = 0; i < industries.length; i++) {
-        var mIndustry = response['data'][industries[i]];
-        mIndustries.add(new Industry(name: industries[i], funding: mIndustry['funding'], startups: mIndustry['startups']));
-      }
+      // for (int i = 0; i < industries.length; i++) {
+      //   var mIndustry = response['data'][industries[i]];
+      //   mIndustries.add(new Industry(
+      //       name: industries[i],
+      //       funding: mIndustry['funding'],
+      //       startups: mIndustry['startups']));
+      // }
+      mIndustries = await compute(getInversionsIsolate, response);
       return mIndustries;
     }
   }
+}
+
+List<Country> getCountriesIsolate(List data) {
+  List<Country> mCountries = <Country>[];
+  for (int i = 0; i < data.length; i++) {
+    mCountries.add(new Country(name: data[i]));
+  }
+  return mCountries;
+}
+
+List<Trend> getTrendsIsolate(dynamic response) {
+  var tempTrends = response['data'].keys;
+  List trends = [];
+  for (var mTrend in tempTrends) {
+    trends.add(mTrend);
+  }
+  List<Trend> mTrends = <Trend>[];
+  for (int i = 0; i < trends.length; i++) {
+    var mTrend = response['data'][trends[i]];
+    mTrends.add(Trend(
+        name: trends[i],
+        funding: mTrend['funding'],
+        startups: mTrend['startups']));
+  }
+  return mTrends;
+}
+
+List<Industry> getInversionsIsolate(dynamic response) {
+  var tempIndustries = response['data'].keys;
+  List industries = [];
+  for (var mIndustry in tempIndustries) {
+    industries.add(mIndustry);
+  }
+  List<Industry> mIndustries = <Industry>[];
+  for (int i = 0; i < industries.length; i++) {
+    var mIndustry = response['data'][industries[i]];
+    mIndustries.add(Industry(
+        name: industries[i],
+        funding: mIndustry['funding'],
+        startups: mIndustry['startups']));
+  }
+  return mIndustries;
 }
